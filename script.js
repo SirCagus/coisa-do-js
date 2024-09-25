41c1d3868bc04c85087f616666b98f29fc13221b
SirCagus <141450259+SirCagus@users.noreply.github.com>
2024-09-25 11:32:05 -0300

    sdf

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//alert(" Deu certo; Miguel Afonso Xavier 28 e Anthony Marba 2")
+alert(" Deu certo; Miguel Afonso Xavier 28 e Anthony Marba 2")
 
 //--let Nome_Usuario = prompt("quantas horas vc trabalha?")
 

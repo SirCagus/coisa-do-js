b7e2b0379aaa6c40536c0a900488ecc53902faa3
SirCagus <141450259+SirCagus@users.noreply.github.com>
2024-10-02 12:06:33 -0300

    a

--- script.js
+++ script.js
@@ -41,7 +41,7 @@
 let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
 
 let i = 0
-while (i < length) {
+while (i < estados.length) {
     console.log(i);
     console.log(estados[i]);
     i = i+1;
